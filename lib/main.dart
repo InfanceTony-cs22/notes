8c212c76c89cd4ee489e4ec5704515c463d4c882
Bennyhinn18 <137780675+Bennyhinn18@users.noreply.github.com>
2023-09-21 23:46:05 +0530

    dynamic subject link

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'department_selection_page.dart';
 import 'models.dart';
-import 'sample_data.dart';
+
+Future<List<Department>> fetchDepartments() async {
+  final response = await http.get(Uri.parse('https://github.com/Bennyhinn18/Files/raw/main/data.json'));
+
+  if (response.statusCode == 200) {
+    Iterable jsonResponse = json.decode(response.body);
+    return jsonResponse.map((data) => Department.fromJson(data)).toList();
+  } else {
+    throw Exception('Failed to load departments from JSON');
+  }
+}
+
 
 void main() {
   runApp(StudyMaterialsApp());
@@ -12,10 +25,23 @@ class StudyMaterialsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Study Materials App',
-      initialRoute: '/departments',
-      routes: {
-        '/departments': (context) => DepartmentSelectionPage(departments: departments),
-      },
+      home: FutureBuilder<List<Department>>(
+        future: fetchDepartments(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator(); // or any loading widget you prefer
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            List<Department>? departments = snapshot.data;
+            if (departments != null) {
+              return DepartmentSelectionPage(departments: departments);
+            } else {
+              return Text('No data available');
+            }
+          }
+        },
+      ),
     );
   }
 }
--- lib/models.dart
+++ lib/models.dart
@@ -3,6 +3,17 @@ class Department {
   final List<Semester> semesters;
 
   Department({required this.name, required this.semesters});
+
+  factory Department.fromJson(Map<String, dynamic> json) {
+    List<Semester> semesters = (json['semesters'] as List)
+        .map((semesterJson) => Semester.fromJson(semesterJson))
+        .toList();
+
+    return Department(
+      name: json['name'] as String,
+      semesters: semesters,
+    );
+  }
 }
 
 class Semester {
@@ -10,6 +21,17 @@ class Semester {
   final List<Subject> subjects;
 
   Semester({required this.number, required this.subjects});
+
+  factory Semester.fromJson(Map<String, dynamic> json) {
+    List<Subject> subjects = (json['subjects'] as List)
+        .map((subjectJson) => Subject.fromJson(subjectJson))
+        .toList();
+
+    return Semester(
+      number: json['number'] as int,
+      subjects: subjects,
+    );
+  }
 }
 
 class Subject {
@@ -17,4 +39,11 @@ class Subject {
   final String link;
 
   Subject({required this.name, required this.link});
+
+  factory Subject.fromJson(Map<String, dynamic> json) {
+    return Subject(
+      name: json['name'] as String,
+      link: json['link'] as String,
+    );
+  }
 }
